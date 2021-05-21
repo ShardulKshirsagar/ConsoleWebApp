f32af7c1c70d32148c60f02329f4e012d7b3cad2
ShardulKshirsagar <73601007+ShardulKshirsagar@users.noreply.github.com>
2021-05-21 19:12:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("My First Console Project")
+var first_name = "Shardul";
+var last_name = "Kshirsagar";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var square_root = Math.sqrt(16);
+console.log(square_root);
+var max_number = Math.max(15, 10);
+console.log(max_number);
+var min_number = Math.min(15, 10);
+console.log(min_number);
+var power_number = Math.pow(4, 2);
+console.log(power_number);
